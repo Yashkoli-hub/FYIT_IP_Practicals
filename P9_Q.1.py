39c773dfac1c8153d424d856c494ff3aee1b30f1
Yashkoli-hub <yash.koli26@vsit.edu.in>
2026-09-23 11:58:46 +0530

    Add files via upload

---
+++ P9_Q.1.py
@@ -0,0 +1,6 @@
+def display_message () :
+    print("Welcome to Python Functions!")
+
+display_message()
+display_message()
+display_message()
---
+++ P9_Q.6.py
@@ -0,0 +1,9 @@
+def process_list (lst):
+    print("Sum =", sum(lst))
+    print("Largest =", max(lst))
+
+list1 = [10, 20, 30]
+list2 = [5, 15, 25, 35]
+
+process_list(list1)
+process_list(list2)
